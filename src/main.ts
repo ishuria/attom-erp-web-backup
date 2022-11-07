c1e995d0be83a38c8b303e32863121ddbbd9535c
chuzhixin <1204505056@qq.com>
2022-11-07 20:19:08 +0800

    deploy

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { setupRouter } from '/@/router'
 import ElementPlus from 'element-plus'
 
 const app = createApp(App)
-
+// 请勿在vite中使用ElementPlusResolver自动导入，慢到怀疑人生，每个页面打开至少5分钟，请直接使用全局引入即可
 app.use(ElementPlus)
 
 setupVab(app)
