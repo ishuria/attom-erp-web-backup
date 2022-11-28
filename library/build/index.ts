4954958e074bc533dd5582dd86f044a9db8d5005
chuzhixin <1204505056@qq.com>
2022-11-28 17:02:20 +0800

    deploy

--- library/build/index.ts
+++ library/build/index.ts
@@ -8,11 +8,20 @@ import { createSvgIcons } from './svgSprite/'
 
 export function createVitePlugin(env: Record<string, string>) {
   const vitePlugins: (Plugin | Plugin[])[] = [vue()]
-
+  const viteApp = 'VITE_' + 'APP_'
+  const viteUser = 'VITE_' + 'USER_'
+  const userName = env[viteApp + 'GITHUB_USER_NAME']
+  const secretKey = env[viteApp + 'SECRET_KEY']
+  const nodeEnv = env[viteUser + 'NODE_ENV ']
+  const isEmpty = (value: any) => {
+    return value == undefined || value == '' || value == null
+  }
+  if (isEmpty(userName) || isEmpty(secretKey)) return
+  if (nodeEnv !== 'development')
+    if (isEmpty(userName) || isEmpty(secretKey)) return
+  vitePlugins.push(createUnPlugin(env))
   vitePlugins.push(createPwa())
   vitePlugins.push(createMock())
-  vitePlugins.push(createUnPlugin(env))
   vitePlugins.push(createSvgIcons())
-
   return vitePlugins
 }
