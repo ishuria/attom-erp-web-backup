29cbfe3ac97ec42edd79e9d5427ef552b162d75b
libin <2741402338@qq.com>
2023-03-12 17:38:54 +0800

    deploy

--- src/views/index/components/VersionInformation.vue
+++ src/views/index/components/VersionInformation.vue
@@ -97,10 +97,7 @@
       td {
         position: relative;
         padding: 9px 15px !important;
-        overflow: hidden;
         font-size: 14px;
-        text-overflow: ellipsis;
-        white-space: nowrap;
         border: 1px solid #e6e6e6;
 
         i {
