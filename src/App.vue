046c962f6cb83751168f996a879033e011417cec
淋雨的小丸子 <chu1204505056@126.com>
2023-05-28 19:53:42 +0800

    deploy

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,29 @@
+<script setup lang="ts">
+  onMounted(() => {
+    if (
+      location.hostname === 'vue-admin-beautiful.com' ||
+      location.hostname === 'chu1204505056.gitee.io'
+    ) {
+      // 演示地址禁止调试
+      ;(() => {
+        function block() {
+          setInterval(() => {
+            ;(function () {
+              return false
+            })
+              ['constructor']('debugger')
+              ['call']()
+          }, 50)
+        }
+        try {
+          block()
+        } catch (err) {
+          console.log(err)
+        }
+      })()
+    }
+  })
+</script>
 <template>
   <vab-app />
 </template>
