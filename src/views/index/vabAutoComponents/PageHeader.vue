4174e7d3866a776a0d27d91d2314eba17b547cbd
chuzhixin <1204505056@qq.com>
2023-06-13 05:23:07 +0800

    deploy

--- src/views/index/vabAutoComponents/PageHeader.vue
+++ src/views/index/vabAutoComponents/PageHeader.vue
@@ -40,7 +40,11 @@
 </script>
 
 <template>
-  <vab-colorful-card class="page-header" shadow="never" :style="style">
+  <vab-colorful-card
+    class="page-header hidden-xs-only"
+    shadow="never"
+    :style="style"
+  >
     <el-avatar class="page-header-avatar" :src="avatar" />
     <div class="page-header-tip">
       <p class="page-header-tip-title">
