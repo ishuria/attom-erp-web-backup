a0fc5a876267a7a6e238c140d154f65d0fd982d4
淋雨的小丸子 <chu1204505056@126.com>
2023-06-23 14:46:20 +0800

    ♻️ refactor: Watermark

--- src/views/vab/watermark/index.vue
+++ src/views/vab/watermark/index.vue
@@ -8,39 +8,31 @@
   </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
   import Watermark from '/@/utils/watermark'
   import { useSettingsStore } from '/@/store/modules/settings'
   import { ElMessageBox } from 'element-plus'
 
-  export default defineComponent({
-    name: 'Watermark',
-    setup() {
-      const settingsStore = useSettingsStore()
-      const { title } = storeToRefs(settingsStore)
-
-      const setWatermark = (value: string) => {
-        //@ts-ignore
-        Watermark.set(value)
-      }
-      const setCustomWatermark = () => {
-        ElMessageBox.prompt('请输入自定义水印', '温馨提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-        })
-          .then(({ value }: any) => {
-            if (value) setWatermark(value)
-          })
-          .catch(() => {})
-      }
-
-      return {
-        title,
-        setWatermark,
-        setCustomWatermark,
-      }
-    },
+  defineOptions({
+    value: 'Watermark',
   })
+  const settingsStore = useSettingsStore()
+  const { title } = storeToRefs(settingsStore)
+
+  const setWatermark = (value: string) => {
+    //@ts-ignore
+    Watermark.set(value)
+  }
+  const setCustomWatermark = () => {
+    ElMessageBox.prompt('请输入自定义水印', '温馨提示', {
+      confirmButtonText: '确定',
+      cancelButtonText: '取消',
+    })
+      .then(({ value }) => {
+        if (value) setWatermark(value)
+      })
+      .catch(() => {})
+  }
 </script>
 
 <style lang="scss" scoped></style>
