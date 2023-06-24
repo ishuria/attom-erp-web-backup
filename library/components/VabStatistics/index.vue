51e76a553c626bb110494fce6f6df2a7e6f8c15b
淋雨的小丸子 <chu1204505056@126.com>
2023-06-25 07:30:06 +0800

    ♻️ refactor: VabStatistics

--- library/components/VabStatistics/index.vue
+++ library/components/VabStatistics/index.vue
@@ -5,9 +5,11 @@
   // 网站访问量统计 如不需要请自行注释
   onMounted(() => {
     if (
-      location.hostname !== 'localhost' ||
-      location.hostname !== '127.0.0.1'
+      location.hostname !== 'localhost' &&
+      !location.hostname.includes('127') &&
+      !location.hostname.includes('192')
     ) {
+      alert(location.hostname)
       ;(function () {
         const hm = document.createElement('script')
         hm.src = 'https://hm.baidu.com/hm.js?820b686671af452e8a4e18952ce946d8'
