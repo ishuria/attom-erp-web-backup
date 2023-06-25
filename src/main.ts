edb12c1382b0627a06241fa196db35e94c99def9
淋雨的小丸子 <chu1204505056@126.com>
2023-06-25 20:40:48 +0800

    ♻️ refactor: main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import { createApp } from 'vue'
 import App from './App.vue'
 
 import { setupVab } from '~/library'
@@ -7,7 +6,6 @@ import { setupRouter } from '/@/router'
 import ElementPlus from 'element-plus'
 
 const app = createApp(App)
-// 请勿在vite中使用ElementPlusResolver自动导入，慢到怀疑人生，每个页面打开至少5分钟，请直接使用全局引入即可
 app.use(ElementPlus)
 
 setupVab(app)
