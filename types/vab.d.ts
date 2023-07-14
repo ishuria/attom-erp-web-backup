1576baae3915062b094ab4361b1914c508e8e2f5
chuzhixin <1204505056@qq.com>
2023-07-14 18:28:52 +0800

    🐛 fix: i18n

--- types/vab.d.ts
+++ types/vab.d.ts
@@ -1,5 +1,5 @@
 declare module 'vite-plugin-unplugin'
 declare module 'vite-plugin-vitebar'
 declare module 'vue-json-viewer'
-declare module 'element-plus/dist/locale/en'
-declare module 'element-plus/dist/locale/zh-cn'
+declare module 'element-plus/dist/locale/en.mjs'
+declare module 'element-plus/dist/locale/zh-cn.mjs'
