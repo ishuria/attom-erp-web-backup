a37d996a09d774d8fb725791fd71ed3e3d446e59
zhideyiman <chu1204505056@126.com>
2023-08-01 13:37:02 +0800

    ♻️ refactor: Pwa

--- library/build/pwa/index.ts
+++ library/build/pwa/index.ts
@@ -1,45 +1,45 @@
 import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
 
-export function createPwa() {
-  const pwaOptions: Partial<VitePWAOptions> = {
-    base: './',
-    registerType: 'autoUpdate', // prompt、autoUpdate
-    workbox: {
-      cleanupOutdatedCaches: true,
-    },
-    manifest: {
-      lang: 'zh',
-      name: 'Vue Shop Vite',
-      short_name: 'Vue Shop Vite',
-      description: 'Vue Shop Vite官网、文档、演示地址',
-      background_color: '#ffffff',
-      theme_color: '#ffffff',
-      icons: [
-        {
-          src: 'pwa-64x64.png',
-          sizes: '64x64',
-          type: 'image/png',
-        },
-        {
-          src: 'pwa-192x192.png',
-          sizes: '192x192',
-          type: 'image/png',
-        },
-        {
-          src: 'pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'any',
-        },
-        {
-          src: 'maskable-icon-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'maskable',
-        },
-      ],
-    },
-  }
+const pwaOptions: Partial<VitePWAOptions> = {
+  base: './',
+  registerType: 'autoUpdate', // prompt、autoUpdate
+  workbox: {
+    cleanupOutdatedCaches: true,
+  },
+  manifest: {
+    lang: 'zh',
+    name: 'Vue Shop Vite',
+    short_name: 'Vue Shop Vite',
+    description: 'Vue Shop Vite官网、文档、演示地址',
+    background_color: '#ffffff',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: 'pwa-64x64.png',
+        sizes: '64x64',
+        type: 'image/png',
+      },
+      {
+        src: 'pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: 'maskable-icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+    ],
+  },
+}
 
+export function createPwa() {
   return VitePWA(pwaOptions)
 }
