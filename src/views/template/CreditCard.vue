f137fd9c4ec0b4fe380cefc739623ca70293b30a
zhideyiman <chu1204505056@126.com>
2023-12-13 18:51:16 +0800

    ♻️ refactor:  global-animation-disabled

--- src/views/template/CreditCard.vue
+++ src/views/template/CreditCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="credit-card-container">
+  <div class="credit-card-container global-animation-disabled">
     <tiny-credit-card-form :background-image="backgroundImage" :form-data="formData" @submit="handleSubmit" />
   </div>
 </template>
