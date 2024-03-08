d0459b1bc71c35e3527fb85a17a5f00a8ed77d7b
zxwk <chu1204505056@126.com>
2024-03-08 22:40:50 +0800

    ⬆️ chore: update version

--- src/views/vab/form/timePicker.vue
+++ src/views/vab/form/timePicker.vue
@@ -1,17 +1,14 @@
 <template>
   <div class="time-picker-container no-background-container">
-    <vab-card>
-      <template #header>固定时间点</template>
+    <vab-card title="固定时间点">
       <el-time-select v-model="value" end="18:30" placeholder="选择时间" start="08:30" step="00:15" />
     </vab-card>
-    <vab-card>
-      <template #header>固定时间范围</template>
+    <vab-card title="固定时间范围">
       <el-time-select v-model="startTime" end="18:30" placeholder="开始时间" start="08:30" step="00:15" style="margin-right: 10px" />
       <el-time-select v-model="endTime" end="18:30" :min-time="startTime" placeholder="结束时间" start="08:30" step="00:15" />
     </vab-card>
 
-    <vab-card>
-      <template #header>任意时间点</template>
+    <vab-card title="任意时间点">
       <el-time-picker
         v-model="value1"
         :disabled-hours="disabledHours"
@@ -58,3 +55,13 @@ const disabledSeconds: any = (hour: number, minute: number) => {
   }
 }
 </script>
+
+<style scoped>
+.time-picker-container {
+  :deep() {
+    .el-select {
+      width: 165px;
+    }
+  }
+}
+</style>
