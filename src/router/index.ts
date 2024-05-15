2b55f20aaf233cffc579491398ca5c777e87806f
zxwk <chu1204505056@126.com>
2024-05-15 17:14:39 +0800

    🐛 fix: route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1642,7 +1642,8 @@ export const setupRouter = (app: App<Element>) => {
   if (authentication === 'intelligence') addRouter(asyncRoutes)
   // 使用后端路由时防止出现[Vue Router warn]: No match found for location with path "/index"报黄，未经全面测试请谨慎使用，建议注释掉else if中的代码
   else if (authentication === 'all' && isHashRouterMode) {
-    const path = window.location.hash.slice(1)
+    let path = window.location.hash.slice(1)
+    if (path === '/') path = '/index'
     const words = path.split('/')
     const lastWord: any = words.at(-1)
     const name = lastWord.charAt(0).toUpperCase() + lastWord.slice(1)
