5c26aa95ac426a35ffc4821de1192d7450eb02d4
zxwk <chu1204505056@126.com>
2024-07-15 13:27:28 +0800

    ♻️ refactor: code

--- src/views/portal/Portal.vue
+++ src/views/portal/Portal.vue
@@ -8,13 +8,13 @@
           <el-col :span="6">
             <div class="left-tab">
               <el-menu active-text-color="var(--el-color-white)" background-color="#39364d" text-color="var(--el-color-white)">
-                <el-menu-item index="1" @click="openWindow('//vuejs-core.cn/admin-pro>')">
+                <el-menu-item index="1" @click="openWindow('//vuejs-core.cn/admin-pro')">
                   <template #title>Vue Admin Pro：企业级中后台前端框架</template>
                 </el-menu-item>
-                <el-menu-item index="2" @click="openWindow('//vuejs-core.cn/admin-plus>')">
+                <el-menu-item index="2" @click="openWindow('//vuejs-core.cn/admin-plus')">
                   <template #title>Vue Admin Plus：企业级中后台前端框架</template>
                 </el-menu-item>
-                <el-menu-item index="3" @click="openWindow('//vuejs-core.cn/shop-vite>')">
+                <el-menu-item index="3" @click="openWindow('//vuejs-core.cn/shop-vite')">
                   <template #title>Vue Shop Vite：全新一代前端模板</template>
                 </el-menu-item>
               </el-menu>
@@ -22,8 +22,8 @@
           </el-col>
           <el-col :span="18">
             <el-carousel arrow="always" height="420px" :interval="3000" @change="handleChange">
-              <el-carousel-item @click="openWindow('//vuejs-core.cn/admin-plus>')" />
-              <el-carousel-item @click="openWindow('//vuejs-core.cn/shop-vite>')" />
+              <el-carousel-item @click="openWindow('//vuejs-core.cn/admin-plus')" />
+              <el-carousel-item @click="openWindow('//vuejs-core.cn/shop-vite')" />
               <el-carousel-item @click="openWindow('//vuejs-core.cn/admin-pro')" />
             </el-carousel>
           </el-col>
@@ -76,8 +76,8 @@
       </main>
       <main>
         <el-carousel arrow="always" class="hidden-sm-and-up" height="200px" :interval="3000" style="margin-top: 70px">
-          <el-carousel-item @click="openWindow('//vuejs-core.cn/admin-plus>')" />
-          <el-carousel-item @click="openWindow('//vuejs-core.cn/shop-vite>')" />
+          <el-carousel-item @click="openWindow('//vuejs-core.cn/admin-plus')" />
+          <el-carousel-item @click="openWindow('//vuejs-core.cn/shop-vite')" />
           <el-carousel-item @click="openWindow('//vuejs-core.cn/admin-pro')" />
         </el-carousel>
         <el-row :gutter="20">
