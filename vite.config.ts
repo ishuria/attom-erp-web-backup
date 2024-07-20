b2f316ed53fb758c9d6f48d6a46ca8f9c867ab59
zxwk <chu1204505056@126.com>
2024-07-20 13:30:22 +0800

    ♻️ refactor: code

--- vite.config.ts
+++ vite.config.ts
@@ -74,8 +74,8 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
           entryFileNames: outputHash ? 'static/js/[name]-[hash].js' : 'static/js/[name].js',
           assetFileNames: outputHash ? 'static/[ext]/[name]-[hash].[ext]' : 'static/[ext]/[name].[ext]',
           manualChunks: {
-            'vsc-element-plus': ['element-plus'],
-            'vsc-nprogress': ['nprogress'],
+            'vsv-element-plus': ['element-plus'],
+            'vsv-nprogress': ['nprogress'],
             'vsv-icon': ['vsv-icon'],
             'vsv-echarts': ['echarts'],
           },
