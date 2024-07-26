3afa343fd0f695be0726f33f8da64c87949a4039
zxwk <chu1204505056@126.com>
2024-07-26 23:47:39 +0800

    ⬆️ chore: update version

--- write.version.js
+++ write.version.js
@@ -6,19 +6,25 @@ const fs = require('node:fs')
 const AdmZip = require('adm-zip')
 const package = require('./package.json')
 const dayjs = require('dayjs')
+const zipFilePath = './dist/dist.zip'
 const data = {
   version: package.version,
 }
 
 fs.writeFile('./public/vue-shop-vite-version.json', `${JSON.stringify(data)}`, (err) => {
-  if (err) console.log(`版本号写入失败！${err.message}`)
-  console.log(`版本号写入成功！`)
-  const zipFilePath = './dist/dist.zip'
-  if (fs.existsSync(zipFilePath)) fs.removeSync(zipFilePath)
-  const zip = new AdmZip()
-  zip.addLocalFolder('./dist')
-  zip.writeZip(zipFilePath)
-  console.log(`dist压缩成功！`)
-  const lastBuildTime = dayjs().format('YYYY-MM-DD HH:mm:ss')
-  console.log(lastBuildTime)
+  if (err) console.log(`public版本号写入失败！${err.message}`)
+  console.log(`dist版本号写入成功！`)
 })
+
+fs.writeFile('./dist/vue-shop-vite-version.json', `${JSON.stringify(data)}`, (err) => {
+  if (err) console.log(`dist版本号写入失败！${err.message}`)
+  console.log(`dist版本号写入成功！`)
+})
+
+if (fs.existsSync(zipFilePath)) fs.removeSync(zipFilePath)
+const zip = new AdmZip()
+zip.addLocalFolder('./dist')
+zip.writeZip(zipFilePath)
+console.log(`dist压缩成功！`)
+const lastBuildTime = dayjs().format('YYYY-MM-DD HH:mm:ss')
+console.log(lastBuildTime)
