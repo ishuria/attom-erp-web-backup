7eb44d541eaea32a2f0762f820ab1f37fe748b1f
zxwk <chu1204505056@126.com>
2024-08-06 16:18:03 +0800

    ♻️ refactor: code

--- library/components/VabTheme/components/VabThemeDrawer.vue
+++ library/components/VabTheme/components/VabThemeDrawer.vue
@@ -50,7 +50,7 @@
             <el-option v-for="item in tabsBarStyleList" :key="item.value" :label="translate(item.label)" :value="item.value" />
           </el-select>
         </el-form-item>
-        <el-form-item v-if="theme.showTabs" :label="translate('标签拖拽')">
+        <el-form-item v-if="device !== 'mobile' && theme.showTabs" :label="translate('标签拖拽')">
           <el-switch v-model="theme.tabDrag" />
         </el-form-item>
         <el-form-item :label="translate('页脚')">
