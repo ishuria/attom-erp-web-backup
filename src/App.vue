4855463bdc7f92a21149787e739ddc02d37c6578
zxwk <chu1204505056@126.com>
2024-08-09 00:21:02 +0800

    ♻️ refactor: code

--- src/App.vue
+++ src/App.vue
@@ -39,17 +39,19 @@ onMounted(() => {
     // 等待动画处理完成再加载页面
     show.value = true
     // 是否允许生产环境进行代码调试，请前往config/cli.config.ts文件配置
-    if (
-      !location.hostname.includes('127') &&
-      !location.hostname.includes('localhost') &&
-      (location.hostname.includes('vuejs-core') || disableDebugger) &&
-      route.query &&
-      route.query.debugger !== 'auto'
-    )
-      DisableDevtool({
-        url: 'https://vuejs-core.cn/debugger',
-        timeOutUrl: 'https://vuejs-core.cn/debugger',
-      })
+    setTimeout(() => {
+      if (
+        !location.hostname.includes('127') &&
+        !location.hostname.includes('localhost') &&
+        (location.hostname.includes('vuejs-core') || disableDebugger) &&
+        route.query &&
+        route.query.debugger !== 'auto'
+      )
+        DisableDevtool({
+          url: 'https://vuejs-core.cn/debugger',
+          timeOutUrl: 'https://vuejs-core.cn/debugger',
+        })
+    }, 500)
   })
 })
 </script>
