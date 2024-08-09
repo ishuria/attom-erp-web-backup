c74a7f3d553a8eea58150bdfdc9dafe75064aed2
zxwk <chu1204505056@126.com>
2024-08-09 15:22:06 +0800

    ♻️ refactor: code

--- library/components/VabTheme/components/VabThemeDrawer.vue
+++ library/components/VabTheme/components/VabThemeDrawer.vue
@@ -50,9 +50,10 @@
             <el-option v-for="item in tabsBarStyleList" :key="item.value" :label="translate(item.label)" :value="item.value" />
           </el-select>
         </el-form-item>
-        <el-form-item v-if="device !== 'mobile' && theme.showTabs" :label="translate('标签拖拽')">
+        <!-- 有bug请勿使用，后续能用会放开-->
+        <!-- <el-form-item v-if="device !== 'mobile' && theme.showTabs" :label="translate('标签拖拽')">
           <el-switch v-model="theme.tabDrag" />
-        </el-form-item>
+        </el-form-item> -->
         <el-form-item :label="translate('页脚')">
           <el-switch v-model="theme.showFooter" @change="handleShowFooter" />
         </el-form-item>
