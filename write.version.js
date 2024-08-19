6d930792394a6d408e92e655538497892b544c37
FlowPeakFish <i@liufengyu.cn>
2024-08-19 19:03:31 +0800

    👷 Add or update CI build system.
    替换set和export命令，在构建脚本中使用cross-env设置环境变量。这提高了跨平台兼容性，并减少了在不同操作系统上执行构建命令时的差异。fix(write.version.js): 优化文件写入和压缩逻辑
    
    在write.version.js脚本中，promisify了fs模块以利用async/await语法，从而提高代码的可读性和错误处理能力。此外，优化了版本文件的写入逻辑，并确保在构建压缩包之前删除旧的压缩包，防止不必要的文件冲突。

--- write.version.js
+++ write.version.js
@@ -2,29 +2,58 @@
  * @description: 演示地址自动生成版本号及压缩包
  * @author sundan
  */
-const fs = require('node:fs')
+const fs = require('node:fs').promises
+const path = require('node:path')
 const AdmZip = require('adm-zip')
 const package = require('./package.json')
 const dayjs = require('dayjs')
-const zipFilePath = './dist/dist.zip'
+
+const publicVersionFilePath = path.join('./public', 'vue-shop-vite-version.json')
+const distVersionFilePath = path.join('./dist', 'vue-shop-vite-version.json')
+const zipFilePath = path.join('./dist', 'dist.zip')
+
 const data = {
   version: package.version,
 }
 
-fs.writeFile('./public/vue-shop-vite-version.json', `${JSON.stringify(data)}`, (err) => {
-  if (err) console.log(`public版本号写入失败！${err.message}`)
-  console.log(`dist版本号写入成功！`)
-})
+async function writeVersionFile(filePath) {
+  try {
+    await fs.writeFile(filePath, JSON.stringify(data, null, 2))
+    console.log(`${filePath} 版本号写入成功！`)
+  } catch (error) {
+    console.error(`${filePath} 版本号写入失败！`, error.message)
+  }
+}
 
-fs.writeFile('./dist/vue-shop-vite-version.json', `${JSON.stringify(data)}`, (err) => {
-  if (err) console.log(`dist版本号写入失败！${err.message}`)
-  console.log(`dist版本号写入成功！`)
+async function createZipArchive() {
+  try {
+    if (
+      await fs
+        .access(zipFilePath)
+        .then(() => true)
+        .catch(() => false)
+    ) {
+      await fs.unlink(zipFilePath)
+    }
+    const zip = new AdmZip()
+    zip.addLocalFolder('./dist')
+    zip.writeZip(zipFilePath)
+    console.log(`dist 压缩成功！`)
+  } catch (error) {
+    console.error(`dist 压缩失败！`, error.message)
+  }
+}
 
-  if (fs.existsSync(zipFilePath)) fs.removeSync(zipFilePath)
-  const zip = new AdmZip()
-  zip.addLocalFolder('./dist')
-  zip.writeZip(zipFilePath)
-  console.log(`dist压缩成功！`)
-  const lastBuildTime = dayjs().format('YYYY-MM-DD HH:mm:ss')
-  console.log(lastBuildTime)
-})
+async function main() {
+  try {
+    await writeVersionFile(publicVersionFilePath)
+    await writeVersionFile(distVersionFilePath)
+    await createZipArchive()
+    const lastBuildTime = dayjs().format('YYYY-MM-DD HH:mm:ss')
+    console.log(`最后构建时间: ${lastBuildTime}`)
+  } catch (error) {
+    console.error('执行脚本时发生错误:', error.message)
+  }
+}
+
+main()
