f243be92f1de1c3a14ab1850d47c2f387257c658
zxwk <chu1204505056@126.com>
2024-08-25 23:17:28 +0800

    ♻️ refactor: bing

--- src/store/modules/bing.ts
+++ src/store/modules/bing.ts
@@ -3,21 +3,35 @@
  * @author sundan
  */
 
+import dayjs from 'dayjs'
+const today = dayjs().format('YYYY-MM-DD')
+
 export const useBingStore = defineStore('bing', {
   state: (): BingModuleType => ({
-    backgroundList: [],
+    backgroundList: JSON.parse(localStorage.getItem(`backgroundList-${today}`) as string) || [],
   }),
   getters: {
     getBackgroundList: (state) => state.backgroundList,
   },
   actions: {
     async setBackgroundList() {
-      await axios({
-        url: `https://api.vuejs-core.cn/getBingImage`,
-        method: 'get',
-      }).then(({ data }) => {
-        this.backgroundList = data.data
-      })
+      /**
+       * @description 必应壁纸每天只允许请求一次,减轻服务器压力
+       * @author sundan
+       */
+      if (!JSON.parse(localStorage.getItem(`backgroundList-${today}`) as string)) {
+        await axios({
+          url: `https://api.vuejs-core.cn/getBingImage`,
+          method: 'get',
+        }).then(({ data }) => {
+          this.backgroundList = data.data
+          const keys = Object.keys(localStorage)
+          keys.forEach((key) => {
+            if (key.startsWith('backgroundList')) localStorage.removeItem(key)
+          })
+          localStorage.setItem(`backgroundList-${today}`, JSON.stringify(data.data))
+        })
+      }
     },
   },
 })
