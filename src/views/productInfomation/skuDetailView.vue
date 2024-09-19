824165469aec409039820a138d363e88096d9438
xiangkai.luo <xiangkai.luo@yunzhou.design>
2024-09-19 16:51:48 +0800

    feat：添加SKU详情页面

---
+++ src/views/productInfomation/skuDetailView.vue
@@ -0,0 +1,28 @@
+<tempalte>
+   <div class="step-form-container">
+        <el-page-header  @back="goBack" style="margin-bottom: 0px;">
+            <template #content>
+                <div class="flex items-center">
+                    <span> <strong> SKU详情 </strong></span>
+                </div>
+            </template>
+        </el-page-header>
+    </div>
+</tempalte>
+
+<script lang="ts" setup>
+import { useTabsStore } from '/@/store/modules/tabs'
+import { handleActivePath } from '/@/utils/routes'
+
+const route: any = useRoute()
+const tabsStore = useTabsStore()
+const { delVisitedRoute } = tabsStore
+
+// back
+const goBack = async () => {
+  await delVisitedRoute(handleActivePath(route, true))
+  history.back()
+}
+
+
+</script>
