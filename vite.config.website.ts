f966b2e5fc232428a31492469ac8bbf73b4f66b4
sundan <chu1204505056@126.com>
2024-12-24 11:37:42 +0800

    ⬆️ chore: update version

--- vite.config.website.ts
+++ vite.config.website.ts
@@ -101,7 +101,7 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
       preprocessorOptions: {
         scss: {
           api: 'modern-compiler', // 修复警告: Deprecation Warning: The legacy JS API is deprecated and will be removed in Dart Sass 2.0.0.
-          sassOptions: { outputStyle: 'expanded' },
+          // sassOptions: { outputStyle: 'expanded' },
         },
       },
       devSourcemap: true,
