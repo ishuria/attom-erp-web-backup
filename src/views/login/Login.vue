4b30454029da8206fa58a9e8e1de444733f5cc62
xiangkai.luo <xiangkai.luo@yunzhou.design>
2025-01-20 21:02:34 +0800

    feat：登录去账号

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -155,7 +155,7 @@ const changeCode = () => {
 }
 
 onBeforeMount(() => {
-  form.username = '1835857596@qq.com'
+  form.username = ''
   form.password = ''
   // 为了演示效果，会在官网演示页自动登录到首页，正式开发可删除
   if (location.hostname.includes('vuejs-core')) {
