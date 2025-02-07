00995f7eba1e3eb6b383723724042a2f99f3eaea
xiangkai.luo <xiangkai.luo@yunzhou.design>
2025-02-07 19:48:43 +0800

    feat:菜单点击修改,取消修改,点击添加菜单保存调用了修改接口bug修复

--- src/views/system/menu/vabAutoComponents/MenuEdit.vue
+++ src/views/system/menu/vabAutoComponents/MenuEdit.vue
@@ -1,23 +1,22 @@
 <template>
-  <vab-dialog v-model="dialogFormVisible" append-to-body :title="title" width="850px" :draggable="false" @close="close">
-    <el-form ref="formRef" label-width="120px" :model="form" :rules="rules" label-position="right">
+  <vab-dialog v-model="dialogFormVisible" append-to-body :draggable="false" :title="title" width="850px" @close="close">
+    <el-form ref="formRef" label-position="right" label-width="120px" :model="form" :rules="rules">
       <el-form-item label="上级菜单" prop="pid">
         <el-tree-select
           v-model="form.pid"
+          :check-strictly="true"
+          clearable
           :data="menuList"
           :filter-node-method="filterNodeMethod"
           filterable
-          clearable
-          :check-strictly="true"
           value-key="id"
           @change="changePid"
           @clear="cleanSelectData"
         />
       </el-form-item>
       <el-row :gutter="24" type="flex">
-
         <el-col :span="24">
-          <el-form-item label="菜单类型" prop="type" >
+          <el-form-item label="菜单类型" prop="type">
             <el-radio-group v-model="form.type">
               <el-radio value="0">菜单</el-radio>
               <el-radio value="1">按钮</el-radio>
@@ -25,18 +24,13 @@
           </el-form-item>
         </el-col>
 
-        <el-col :span="11" class="menu-row">
+        <el-col class="menu-row" :span="11">
           <el-form-item label="菜单排序" prop="sort">
-            <el-input-number
-              v-model="form.sort"
-              :min="0"
-              :max="100"
-              @change="handleChange"
-            />
+            <el-input-number v-model="form.sort" :max="100" :min="0" @change="handleChange" />
           </el-form-item>
         </el-col>
 
-        <el-col :span="13" class="menu-row">
+        <el-col class="menu-row" :span="13">
           <el-form-item label="菜单状态" prop="status">
             <el-radio-group v-model="form.status">
               <el-radio value="0">正常</el-radio>
@@ -45,69 +39,68 @@
           </el-form-item>
         </el-col>
 
-        <el-col :span="11" v-if="form.type == 0">
-          <el-form-item label="name" prop="name" class="menu-row">
+        <el-col v-if="form.type == 0" :span="11">
+          <el-form-item class="menu-row" label="name" prop="name">
             <el-input v-model="form.name" clearable />
           </el-form-item>
         </el-col>
 
-        <el-col :span="13" v-if="form.type == 0">
-          <el-form-item label="路径" prop="path" class="menu-row">
+        <el-col v-if="form.type == 0" :span="13">
+          <el-form-item class="menu-row" label="路径" prop="path">
             <el-input v-model="form.path" clearable />
           </el-form-item>
         </el-col>
 
-        <el-col :span="11" class="menu-row">
-          <el-form-item label="标题" prop="meta.title" v-if="form.type == 0">
+        <el-col class="menu-row" :span="11">
+          <el-form-item v-if="form.type == 0" label="标题" prop="meta.title">
             <el-input v-model="form.meta.title" clearable />
           </el-form-item>
-          <el-form-item label="按钮名称" prop="meta.title" v-if="form.type == 1">
+          <el-form-item v-if="form.type == 1" label="按钮名称" prop="meta.title">
             <el-input v-model="form.meta.title" clearable />
           </el-form-item>
         </el-col>
 
-        <el-col v-if="form.type != 0" :span="13" class="menu-row">
+        <el-col v-if="form.type != 0" class="menu-row" :span="13">
           <el-form-item label="后台权限id" prop="permissionId">
             <el-input v-model="form.permissionId" clearable />
           </el-form-item>
         </el-col>
       </el-row>
 
-      <el-form-item label="图标" class="menu-row" v-if="form.type == 0">
+      <el-form-item v-if="form.type == 0" class="menu-row" label="图标">
         <el-popover popper-class="icon-selector-popper" trigger="click" :width="500">
           <template #reference>
-            <el-input v-model="form.meta.icon" clearable placeholder="点击选择图标"/>
+            <el-input v-model="form.meta.icon" clearable placeholder="点击选择图标" />
           </template>
-           <el-row :gutter="20" >
+          <el-row :gutter="20">
             <el-col :span="24">
               <el-form inline @submit.prevent>
                 <el-form-item>
-                  <el-input clearable  v-model="iconTitle"/>
+                  <el-input v-model="iconTitle" clearable />
                 </el-form-item>
                 <el-form-item>
-                <el-button :icon="Search" native-type="submit" type="primary" @click="queryData" />
-              </el-form-item>
+                  <el-button :icon="Search" native-type="submit" type="primary" @click="queryData" />
+                </el-form-item>
               </el-form>
             </el-col>
-           </el-row>
-           <div style="display: flex;flex-wrap: wrap;justify-content: space-around;max-height: 300px;overflow-y: auto;">
+          </el-row>
+          <div style="display: flex; flex-wrap: wrap; justify-content: space-around; max-height: 300px; overflow-y: auto">
             <div v-for="(item, index) in queryIcon" :key="index" :span="6">
               <vab-card @click="handleIcon(item)">
                 <vab-icon :icon="item" />
               </vab-card>
             </div>
-           </div>
+          </div>
         </el-popover>
       </el-form-item>
 
-      <el-form-item label="vue文件路径" prop="component" v-if="form.pid == '1' && form.type != 1">
+      <el-form-item v-if="form.pid == '1' && form.type != 1" label="vue文件路径" prop="component">
         <el-input v-model="form.component" :disabled="true" />
       </el-form-item>
 
-      <el-form-item label="vue文件路径" prop="component" v-if="form.pid != '1' && form.type != 1">
+      <el-form-item v-if="form.pid != '1' && form.type != 1" label="vue文件路径" prop="component">
         <el-input v-model="form.component" clearable />
       </el-form-item>
-
     </el-form>
     <template #footer>
       <el-button type="primary" @click="save">保存</el-button>
@@ -116,36 +109,33 @@
 </template>
 
 <script lang="ts" setup>
-import type { FormInstance } from 'element-plus'
 import { Search } from '@element-plus/icons-vue'
-import { 
-  getMenuNameList,
-  doAdd,
-  doUpdate
-   } from '/@/api/devlocal/router'
-import {icons} from '/@/icon'
+import type { FormInstance } from 'element-plus'
+import { ref } from 'vue'
+import { doAdd, getMenuNameList } from '/@/api/devlocal/router'
+import { icons } from '/@/icon'
 
-import { IMenuAddOrUpdteReq } from '/@/type/menu/menuType'
+import type { IMenuAddOrUpdteReq } from '/@/type/menu/menuType'
 
 defineOptions({
   name: 'MenuEdit',
 })
 const queryIcon = ref<any>([])
-const iconTitle = ref();
+const iconTitle = ref()
 const emit = defineEmits(['fetch-data'])
 
 const formRef = ref<FormInstance>()
 
 const form = reactive<IMenuAddOrUpdteReq>({
-  id:"",
+  id: '',
   pid: '',
   name: '',
   path: '',
   component: '',
-  permissionId:'',
+  permissionId: '',
   type: 0,
   sort: 0,
-  status: "0",
+  status: '0',
   meta: {
     title: '',
     icon: '',
@@ -159,7 +149,7 @@ const form = reactive<IMenuAddOrUpdteReq>({
     tabHidden: false,
     guard: [],
   },
-  menuName:""
+  menuName: '',
 })
 
 const rules = reactive<any>({
@@ -188,10 +178,10 @@ const showEdit = (row: IMenuAddOrUpdteReq) => {
       Object.assign(form, row)
     } else {
       title.value = '添加'
-      form.component = ""
-      form.permissionId = ""
-      form.name = ""
-      form.path = ""
+      form.component = ''
+      form.permissionId = ''
+      form.name = ''
+      form.path = ''
       form.meta = {
         title: '',
         icon: '',
@@ -216,21 +206,18 @@ defineExpose({
 const close = () => {
   dialogFormVisible.value = false
   emit('fetch-data')
-  setTimeout(() => {
-    formRef.value?.clearValidate()
-    formRef.value?.resetFields()
-  }, 2000)
- 
+  form.id = ''
 }
 
 const save = () => {
   formRef.value?.validate(async (valid: any) => {
     if (valid) {
-      if (form.id && form.id != ""){
-        const { msg }: any = await doUpdate({...form,menuName:form.meta.title})
-        await $baseMessage(msg, 'success', 'hey')
-      }else{
-        const { msg }: any = await doAdd({...form,menuName:form.meta.title})
+      if (form.id && form.id != '') {
+        console.log(form, '修改')
+        // const { msg }: any = await doUpdate({...form,menuName:form.meta.title})
+        // await $baseMessage(msg, 'success', 'hey')
+      } else {
+        const { msg }: any = await doAdd({ ...form, menuName: form.meta.title })
         await $baseMessage(msg, 'success', 'hey')
       }
       await close()
@@ -247,7 +234,7 @@ const menuInit = async () => {
 }
 
 // 上级菜单搜索过滤
-const filterNodeMethod = (value:string, data:any) => data.label.includes(value)
+const filterNodeMethod = (value: string, data: any) => data.label.includes(value)
 
 // 菜单排序
 const handleChange = (value: any) => {
@@ -256,22 +243,22 @@ const handleChange = (value: any) => {
 
 // icon图标过滤方法
 const queryData = () => {
-  if (iconTitle.value == undefined || iconTitle.value == ""){
-    queryIcon.value = icons;
-  }else {
-    queryIcon.value = queryIcon.value.filter((val:string)=> val.includes(iconTitle.value))
+  if (iconTitle.value == undefined || iconTitle.value == '') {
+    queryIcon.value = icons
+  } else {
+    queryIcon.value = queryIcon.value.filter((val: string) => val.includes(iconTitle.value))
   }
 }
 
 // 主目录变化
-const changePid = (value:any) =>{
-  if (value == 1){
-    form.component = "Layout"
+const changePid = (value: any) => {
+  if (value == 1) {
+    form.component = 'Layout'
   }
   form.pid = value
 }
 
-const cleanSelectData = () =>{
+const cleanSelectData = () => {
   form.pid = ''
   form.component = ''
 }
@@ -279,17 +266,16 @@ const cleanSelectData = () =>{
 onBeforeMount(() => {
   menuInit()
 })
-
 </script>
 
 <style lang="scss" scoped>
 :deep() {
   .menu-row {
     padding-top: 25px;
-  } 
+  }
 }
 
-.icon-selector-popper{
+.icon-selector-popper {
   .vab-query-form {
     margin-top: calc(var(--el-margin) / 2);
 
