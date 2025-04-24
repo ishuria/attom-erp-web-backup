113c6090ff7a353998ee8c3640b904f2c886629b
gaoyan.ma <gaoyan.ma@yunzhou.design>
2025-04-24 18:11:43 +0800

    fix: 修改质检历史为tab布局，对接查询接口，去掉质检类型列

--- src/views/packagingShipping/qualityInspectionHistory/index.vue
+++ src/views/packagingShipping/qualityInspectionHistory/index.vue
@@ -1,82 +1,150 @@
 <template>
-  <div class="comprehensive-table-container auto-height-container">
-    <vab-query-form>
-      <vab-query-form-right-panel :span="24">
-        <el-form inline @submit.prevent>
-          <el-form-item>
-            <el-input v-model.trim="queryForm.keyWord" clearable placeholder="请输入搜索关键词" @input="queryData" @keydown.enter="queryData" />
-          </el-form-item>
-          <el-form-item>
-            <el-button :icon="Search" :loading="listLoading" type="primary" @click="queryData" />
-          </el-form-item>
-        </el-form>
-      </vab-query-form-right-panel>
-    </vab-query-form>
-    <el-table 
-      border 
-      :cell-class-name="clearPadding" 
-      :data="fakeData" 
-      :header-cell-style="{ textAlign: 'center' }"
-      stripe
-    >
-      <el-table-column align="center" label="质检日期" prop="date" />
-      <el-table-column align="center" label="PO" prop="po" />
-      <el-table-column label="SKU图片" prop="" width="75">
-        <template #header>
-          SKU<br />图片
-        </template>
-        <template #default="{ row }">
-          <el-image :src="row.skuImgUrl" @click="showImagePreview(row.skuImgUrl)">
-            <template #error><el-icon /></template>
-          </el-image>
-        </template>
-      </el-table-column>
-      <el-table-column label="SKU" >
-        <template #default="{ row }">
-          {{ row.sku }}<br />
-          {{ row.productName }}-{{ row.variantName }}-{{ row.productDesc }}
-        </template>
-      </el-table-column>
-      <el-table-column align="center" label="质检类型" prop="type" >
-        <template #default="{ row }">
-          {{ row.type === 0 ? '新品质检' : '打包质检' }}
-        </template>
-      </el-table-column>
-      <el-table-column align="center" label="质检结论" prop="status" >
-        <template #default="{ row }">
-          <el-tag :type="row.status === 1 ? 'success' : 'danger'">{{ row.status === 1 ? '通过' : '不通过' }}</el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column align="center" label="操作" prop="" >
-        <template #default="{ row }">
-          <el-button text type="primary" @click="handleViewReport(row)">查看报告</el-button>
-        </template>
-      </el-table-column>
-      <template #empty>
-        <el-empty class="vab-data-empty" />
-      </template>
-    </el-table>
-    <vab-pagination
-      :current-page="queryForm.pageNo"
-      :page-size="queryForm.pageSize"
-      :total="total"
-      @current-change="handleCurrentChange"
-      @size-change="handleSizeChange"
-    />
+  <div class="tabs-table-container no-background-container">
+    <el-tabs v-model="activeName" type="border-card" @tab-click="handleTabClick">
+      <el-tab-pane label="新品质检" :name="0">
+        <vab-query-form>
+          <vab-query-form-right-panel :span="24">
+            <el-form inline @submit.prevent>
+              <el-form-item>
+                <el-input v-model.trim="queryForm.keyWord" clearable placeholder="请输入搜索关键词" @input="queryData" @keydown.enter="queryData" />
+              </el-form-item>
+              <el-form-item>
+                <el-button :icon="Search" :loading="listLoading" type="primary" @click="queryData" />
+              </el-form-item>
+            </el-form>
+          </vab-query-form-right-panel>
+        </vab-query-form>
+        <el-table 
+          border 
+          :cell-class-name="clearPadding" 
+          :data="list" 
+          :header-cell-style="{ textAlign: 'center' }"
+          stripe
+        >
+          <el-table-column align="center" label="质检日期" prop="submitDate" />
+          <el-table-column align="center" label="PO" prop="po" />
+          <el-table-column label="SKU图片" prop="" width="75">
+            <template #header>
+              SKU<br />图片
+            </template>
+            <template #default="{ row }">
+              <el-image :src="row.skuImgUrl" @click="showImagePreview(row.skuImgUrl)">
+                <template #error><el-icon /></template>
+              </el-image>
+            </template>
+          </el-table-column>
+          <el-table-column label="SKU" >
+            <template #default="{ row }">
+              {{ row.sku }}<br />
+              {{ row.productName }}-{{ row.variantName }}-{{ row.productDesc }}
+            </template>
+          </el-table-column>
+          <el-table-column align="center" label="质检结论" prop="status" >
+            <template #default="{ row }">
+              <el-tag :type="row.status === 1 ? 'success' : 'danger'">{{ row.status === 1 ? '通过' : '不通过' }}</el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column align="center" label="操作" prop="" >
+            <template #default="{ row }">
+              <el-button text type="primary" @click="handleViewReport(row)">查看报告</el-button>
+            </template>
+          </el-table-column>
+          <template #empty>
+            <el-empty class="vab-data-empty" />
+          </template>
+        </el-table>
+        <vab-pagination
+          :current-page="queryForm.pageNo"
+          :page-size="queryForm.pageSize"
+          :total="total"
+          @current-change="handleCurrentChange"
+          @size-change="handleSizeChange"
+        />
+      </el-tab-pane>
+      <el-tab-pane label="打包质检" :name="1">
+        <vab-query-form>
+          <vab-query-form-right-panel :span="24">
+            <el-form inline @submit.prevent>
+              <el-form-item>
+                <el-input v-model.trim="queryForm.keyWord" clearable placeholder="请输入搜索关键词" @input="queryData" @keydown.enter="queryData" />
+              </el-form-item>
+              <el-form-item>
+                <el-button :icon="Search" :loading="listLoading" type="primary" @click="queryData" />
+              </el-form-item>
+            </el-form>
+          </vab-query-form-right-panel>
+        </vab-query-form>
+        <el-table 
+          border 
+          :cell-class-name="clearPadding" 
+          :data="list" 
+          :header-cell-style="{ textAlign: 'center' }"
+          stripe
+        >
+          <el-table-column align="center" label="质检日期" prop="submitDate" />
+          <el-table-column align="center" label="PO" prop="po" />
+          <el-table-column label="SKU图片" prop="" width="75">
+            <template #header>
+              SKU<br />图片
+            </template>
+            <template #default="{ row }">
+              <el-image :src="row.skuImgUrl" @click="showImagePreview(row.skuImgUrl)">
+                <template #error><el-icon /></template>
+              </el-image>
+            </template>
+          </el-table-column>
+          <el-table-column label="SKU" >
+            <template #default="{ row }">
+              {{ row.sku }}<br />
+              {{ row.productName }}-{{ row.variantName }}-{{ row.productDesc }}
+            </template>
+          </el-table-column>
+          <el-table-column align="center" label="质检结论" prop="status" >
+            <template #default="{ row }">
+              <el-tag :type="row.status === 1 ? 'success' : 'danger'">{{ row.status === 1 ? '通过' : '不通过' }}</el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column align="center" label="操作" prop="" >
+            <template #default="{ row }">
+              <el-button text type="primary" @click="handleViewReport(row)">查看报告</el-button>
+            </template>
+          </el-table-column>
+          <template #empty>
+            <el-empty class="vab-data-empty" />
+          </template>
+        </el-table>
+        <vab-pagination
+          :current-page="queryForm.pageNo"
+          :page-size="queryForm.pageSize"
+          :total="total"
+          @current-change="handleCurrentChange"
+          @size-change="handleSizeChange"
+        />
+      </el-tab-pane>
+    </el-tabs>
     <el-image-viewer v-if="imagePreviewVisible" hide-on-click-modal :url-list="imagePreviewList" @close="closeImagePreview" />
-    
   </div>
 </template>
 
 <script lang="ts" setup>
 import { Search } from '@element-plus/icons-vue'
+import { getQualityInspectionList } from '/@/api/devlocal/packagingShipping'
+import type { IGetQualityInspectionList, IGetQualityInspectionListReq } from '/@/type/packagingShipping/packagingType'
+import type  { TabsPaneContext } from 'element-plus'
 
 defineOptions({
   name: 'QualityInspectionHistory'
 })
 
+const activeName = ref<number>(0)
 const imagePreviewVisible = ref<boolean>(false)
 const imagePreviewList = ref<string[]>([])
+const handleTabClick = (tab: TabsPaneContext) => {
+  const t = Number(tab.props.name)
+  activeName.value = t
+  queryForm.type = t === 0 ? 0 : 1
+  queryData()
+}
 const closeImagePreview = () => {
   imagePreviewVisible.value = false
 }
@@ -86,13 +154,14 @@ const showImagePreview = (url: string) => {
 }
 const fakeData = [{ po: 'PO111111', status: 1 }, { po: 'PO111111', status: 0 }]
 const listLoading = ref<boolean>(false)
-const queryForm = reactive<any>({
+const queryForm = reactive<IGetQualityInspectionListReq>({
   keyWord: '',
   pageNo: 1,
-  pageSize: 20
+  pageSize: 20,
+  type: 0,
 })
 const total = ref<number>(0)
-
+const list = ref<IGetQualityInspectionList[]>([])
 const handleViewReport = (row: any) => {
   //
 }
@@ -111,22 +180,83 @@ const handleSizeChange = (val: number) => {
   queryForm.pageSize = val
   fetchData()
 }
-const fetchData = () => {
-  //
+const fetchData = async () => {
+  listLoading.value = true
+  const { data } = await getQualityInspectionList(queryForm)
+  total.value = data.total
+  list.value = data.list
+  listLoading.value = false
 }
 const queryData = () => {
   queryForm.pageNo = 1
   fetchData()
 }
+onBeforeMount(() => {
+  fetchData()
+})
 </script>
 
 <style lang="scss" scoped>
-.noneHoverTable :deep(.clear-padding) {
-  padding-top: 0;
-  padding-bottom: 0;
-}
-.noneHoverTable :deep(.clear-padding .cell) {
-  padding-right: 0;
-  padding-left: 0;
+// .noneHoverTable :deep(.clear-padding) {
+//   padding-top: 0;
+//   padding-bottom: 0;
+// }
+// .noneHoverTable :deep(.clear-padding .cell) {
+//   padding-right: 0;
+//   padding-left: 0;
+// }
+.tabs-table-container {
+  :deep() {
+    .el-tabs {
+      border-radius: var(--el-border-radius-base);
+
+      &__header {
+        border-top-left-radius: var(--el-border-radius-base);
+        border-top-right-radius: var(--el-border-radius-base);
+      }
+
+      &__nav-wrap {
+        border-radius: var(--el-border-radius-base);
+      }
+
+      .el-tab-pane {
+        display: flex;
+        flex-direction: column;
+        height: calc(var(--el-container-height) - var(--el-padding) - 52px) !important;
+
+        .vab-query-form {
+          .left-panel {
+            margin-bottom: 5px !important;
+          }
+          .el-form {
+            .el-form-item:first-child {
+              margin: 0 !important;
+
+              .el-check-tag,
+              .el-form-item__label {
+                margin: 0 10px 5px 0;
+                border-radius: 99px;
+              }
+            }
+            .el-form-item:last-child {
+              margin: 0 !important;
+            }
+          }
+        }
+
+        .el-table {
+          flex: 1;
+          .clear-padding {
+            padding-top: 0;
+            padding-bottom: 0;
+            .cell {
+              padding-right: 0;
+              padding-left: 0;
+            }
+          }
+        }
+      }
+    }
+  }
 }
 </style>
