5c79b27ff002651c911bf17e4ff6c166cc129118
gaoyan.ma <gaoyan.ma@yunzhou.design>
2025-05-16 19:47:24 +0800

    fix: 修改国旗显示

--- src/views/storeOperations/productPerformanceDashboard/index.vue
+++ src/views/storeOperations/productPerformanceDashboard/index.vue
@@ -357,17 +357,17 @@
                     <span class="rate-count">{{ row.commentsNumbers }}</span>
                     <span style="font-size: 28px;">{{ row.flag }}</span>
                   </div> -->
-                  <!-- <a :href="row.amazonUrl + '#averageCustomerReviewsAnchor'" style=" color: inherit;text-decoration: none;" target="_blank">
+                  <a :href="row.amazonUrl" style=" color: inherit;text-decoration: none;" target="_blank">
                     <div class="rate-wrapper" style="cursor: pointer;">
                       <span class="rate-value">{{ row.rating !== 0 && row.rating != null ? row.rating.toFixed(1) : 0 }}</span>
                       <span>
                         <el-rate v-model="row.displayRating" class="custom-rate" disabled :void-icon="Star" />
                       </span>
                       <span class="rate-count">{{ row.commentsNumbers }}</span>
-                      <span style="font-size: 28px;">{{ row.flag }}</span>
+                      <span style="margin-top: -2px"><country-flag :country='row.flag'/></span>
                     </div>
-                  </a> -->
-                  <div
+                  </a>
+                  <!-- <div
                     class="rate-wrapper"
                     style="cursor: pointer;"
                     @click="goToReview(row.amazonUrl)"
@@ -377,11 +377,11 @@
                       <el-rate v-model="row.displayRating" class="custom-rate" disabled :void-icon="Star" />
                     </span>
                     <span class="rate-count">{{ row.commentsNumbers }}</span>
-                    <span style="font-size: 28px;">{{ row.flag }}</span>
-                  </div>
+                    <span style="margin-top: -2px"><country-flag :country='row.flag'/></span>
+                  </div> -->
                 </span>
                 <span v-if="item.label === 'ASIN'">
-                  <el-link type="primary">{{ row.asin }}</el-link>
+                 {{ row.asin }}
                 </span>
                 <span v-if="item.label === '父体ASIN'">
                   <el-link type="primary">{{ row.parentAsin }}</el-link>
@@ -834,12 +834,20 @@
                 <span class="copySku" data-sku="row.sku" @click="handleClipboard($event, row.asin)" >
                   <vab-icon icon="file-copy-2-fill" />
                 </span>
-                <div class="rate-wrapper">
+                <!-- <div class="rate-wrapper">
                   <span class="rate-value">{{ row.rating !== 0 && row.rating != null ? row.rating.toFixed(1) : 0 }}</span>
                   <span><el-rate v-model="row.displayRating" class="custom-rate" disabled :void-icon="Star" /></span>
                   <span class="rate-count">{{ row.commentsNumbers }}</span>
-                  <span style="font-size: 28px;">{{ row.flag }}</span>
-                </div>
+                  <span style="margin-top: -2px"><country-flag :country='row.flag'/></span>
+                </div> -->
+                <a :href="row.amazonUrl" style=" color: inherit;text-decoration: none;" target="_blank">
+                  <div class="rate-wrapper" style="cursor: pointer;">
+                    <span class="rate-value">{{ row.rating !== 0 && row.rating != null ? row.rating.toFixed(1) : 0 }}</span>
+                    <span><el-rate v-model="row.displayRating" class="custom-rate" disabled :void-icon="Star" /></span>
+                    <span class="rate-count">{{ row.commentsNumbers }}</span>
+                    <span style="margin-top: -2px"><country-flag :country='row.flag'/></span>
+                  </div>
+                </a>
               </span>
               <span v-if="item.label === 'SKU'">
                 <el-tooltip content=" " :disabled="!row.overflow_sku" effect="dark" placement="top">
@@ -1280,7 +1288,7 @@
                   <span class="rate-value">{{ row.rating !== 0 && row.rating != null ? row.rating.toFixed(1) : 0 }}</span>
                   <span><el-rate v-model="row.displayRating" class="custom-rate" disabled :void-icon="Star" /></span>
                   <span class="rate-count">{{ row.commentsNumbers }}</span>
-                  <span style="font-size: 28px;">{{ row.flag }}</span>
+                  <span style="margin-top: -2px"><country-flag :country='row.flag'/></span>
                 </div>
               </span>
               <span v-if="item.label === 'SKU'">
@@ -1487,20 +1495,21 @@ import handleClipboard from '/@/utils/clipboard'
 import { formatPercentage, getAmazonStars, handleImgUrl } from '/@/utils/rate'
 import { _addData } from '/@/utils/skuOptions'
 import { calculateBrColumnWidth, flexColumnWidth, processField, removeHtmlTags } from '/@/utils/tableColum'
+import CountryFlag from 'vue-country-flag-next'
 
 defineOptions({
   name: 'ProductPerformanceDashboard',
 })
 
-const goToReview = (url: string) => {
-  const newWindow = window.open(url, '_blank');
-  if (newWindow) {
-    // 1000ms 延迟，等待页面加载后设置锚点
-    setTimeout(() => {
-      newWindow.location.href = `${newWindow.location.href}#averageCustomerReviewsAnchor`;
-    }, 10000);
-  }
-};
+// const goToReview = (url: string) => {
+//   const newWindow = window.open(`${url}?th=1#averageCustomerReviewsAnchor`, '_blank');
+//   if (newWindow) {
+//     // 1000ms 延迟，等待页面加载后设置锚点
+//     setTimeout(() => {
+//       newWindow.location.href = `${newWindow.location.href}#averageCustomerReviewsAnchor`;
+//     }, 10000);
+//   }
+// };
 // 防抖处理
 const debouncedQueryData = debounce(() => {
   queryData()
