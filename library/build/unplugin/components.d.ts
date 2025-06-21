368fd9d3f3b9d6d53b085803c9ae14c2d2bce3bc
sundan <chu1204505056@126.com>
2025-06-21 22:43:20 +0800

    ⬆️ chore: update version

--- library/build/unplugin/components.d.ts
+++ library/build/unplugin/components.d.ts
@@ -54,7 +54,6 @@ declare module 'vue' {
     ElButton: typeof import('element-plus/es')['ElButton']
     ElButtonGroup: typeof import('element-plus/es')['ElButtonGroup']
     ElCalendar: typeof import('element-plus/es')['ElCalendar']
-    ElCard: typeof import('element-plus/es')['ElCard']
     ElCarousel: typeof import('element-plus/es')['ElCarousel']
     ElCarouselItem: typeof import('element-plus/es')['ElCarouselItem']
     ElCascader: typeof import('element-plus/es')['ElCascader']
