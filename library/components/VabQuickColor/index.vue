8ce7a643c7f6d2691da8020ddbfd31a7e9b1fb91
sundan <chu1204505056@126.com>
2025-07-01 19:50:54 +0800

    ♻️ refactor: code

--- library/components/VabQuickColor/index.vue
+++ library/components/VabQuickColor/index.vue
@@ -40,10 +40,10 @@ const palette = ['#1e90ff', '#4e6ef2', '#3fb884', '#16baa9', '#009688', '#6954f0
         display: flex;
         align-items: center;
         justify-content: center;
-        width: var(--el-margin);
-        height: var(--el-margin);
+        width: 20px;
+        height: 20px;
         cursor: pointer;
-        border-radius: var(--el-border-radius-base);
+        border-radius: var(--el-border-radius-small);
         transition: transform 0.1s;
         &:hover {
             transform: scale(1.1);
