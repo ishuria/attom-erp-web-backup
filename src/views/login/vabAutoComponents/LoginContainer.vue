0b2852add1eab64e288e50c6793c412c5f13c267
sundan <chu1204505056@126.com>
2025-07-09 14:11:39 +0800

    ♻️ refactor: login background

--- src/views/login/vabAutoComponents/LoginContainer.vue
+++ src/views/login/vabAutoComponents/LoginContainer.vue
@@ -1,5 +1,13 @@
 <template>
-    <div class="login-container" :style="{ background: background, backgroundSize: '100%' }">
+    <div
+        class="login-container"
+        :style="{
+            background: background,
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+        }"
+    >
         <div v-show="theme.showLanguage || theme.showColorPicker || theme.showDark" class="login-right-tools">
             <el-checkbox v-model="show" @change="handleShow">
                 {{ translate('必应壁纸') }}
