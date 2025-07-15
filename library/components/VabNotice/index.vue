7a588a253ec8cd42f29390a480d3c89b70a3970b
sundan <chu1204505056@126.com>
2025-07-15 20:32:59 +0800

    ♻️ refactor: code

--- library/components/VabNotice/index.vue
+++ library/components/VabNotice/index.vue
@@ -101,11 +101,11 @@ const colors = [
   },
   { bgColor: 'var(--el-color-info-light-9)', color: 'var(--el-color-info)' },
   {
-    bgColor: '#F2ECFE',
+    bgColor: 'rgba(133, 69, 247, 0.1)',
     color: '#8545F7',
   },
   {
-    bgColor: '#E7F8F6',
+    bgColor: 'rgba(24, 187, 170, 0.1)',
     color: '#18BBAA',
   },
 ]
