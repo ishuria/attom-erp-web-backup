a75d86ed813152833f68c3ab14cf51062c95244b
gaoyan.ma <gaoyan.ma@yunzhou.design>
2025-11-17 11:32:44 +0800

    fix：自动化日志增加Rating列以及修改开关广告显示错误

--- src/views/storeOperations/vabAutoComponents/AutomationSystemOperationLog.vue
+++ src/views/storeOperations/vabAutoComponents/AutomationSystemOperationLog.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <vab-dialog v-model="visible" width="85%">
+    <vab-dialog v-model="visible" width="90%">
       <el-table :cell-style="{ textAlign: 'center' }" :data="list" :header-cell-style="{ textAlign: 'center' }" max-height="60vh" stripe>
-        <el-table-column label="更新时间" min-width="100" prop="updateTime" />
+        <el-table-column label="更新时间" min-width="120" prop="updateTime" />
         <el-table-column label="操作对象" min-width="110" prop="operationGroupName" />
-        <el-table-column label="操作类型" min-width="100" prop="operationTypeName">
+        <el-table-column label="操作类型" min-width="100" prop="operationType">
           <template #default="{ row }">
-            <el-tag :type="row.operationTypeName === 0 ? 'danger' : 'success'">
-              {{ row.operationTypeName === 0 ? '关广告' : '开广告' }}
+            <el-tag :type="row.operationType === 0 ? 'danger' : 'success'">
+              {{ row.operationType === 0 ? '关广告' : '开广告' }}
             </el-tag>
           </template>
         </el-table-column>
@@ -23,33 +23,73 @@
           <el-table-column label="ACOS" min-width="90" prop="operationAcos">
             <template #default="{ row }">{{ row.operationAcos }}%</template>
           </el-table-column>
-          <el-table-column label="剩余可售天数" min-width="110" prop="operationDays" />
+          <el-table-column label="剩余可售天数" min-width="120" prop="operationDays" />
           <el-table-column label="断货天数" min-width="100" prop="operationOutStockDays" />
           <el-table-column label="可售库存数" min-width="110" prop="operationStock" />
           <el-table-column label="毛利率" min-width="90" prop="operationGrossProfit">
             <template #default="{ row }">{{ row.operationGrossProfit }}%</template>
           </el-table-column>
+          <el-table-column label="Rating" min-width="90" prop="operationRating" />
         </el-table-column>
         <el-table-column label="开广告设定">
           <el-table-column label="ACOS≤" min-width="90" prop="openAcos">
-            <template #default="{ row }">{{ row.openAcos }}%</template>
+            <template #default="{ row }">
+              {{ row.operationType === 0 ? '-' : `${row.openAcos}%` }}
+            </template>
           </el-table-column>
-          <el-table-column label="断货天数≤" min-width="100" prop="openOutStockDays" />
-          <el-table-column label="剩余可售天数≥" min-width="120" prop="openDays" />
-          <el-table-column label="可售库存数≥" min-width="110" prop="openStock" />
+          <el-table-column label="断货天数≤" min-width="100" prop="openOutStockDays">
+            <template #default="{ row }">
+              {{ row.operationType === 0 ? '-' : row.openOutStockDays }}
+            </template>
+          </el-table-column>
+          <el-table-column label="剩余可售天数≥" min-width="125" prop="openDays">
+            <template #default="{ row }">
+              {{ row.operationType === 0 ? '-' : row.openDays }}
+            </template>
+          </el-table-column>
+          <el-table-column label="可售库存数≥" min-width="110" prop="openStock">
+            <template #default="{ row }">
+              {{ row.operationType === 0 ? '-' : row.openStock }}
+            </template>
+          </el-table-column>
           <el-table-column label="毛利率≥" min-width="110" prop="openGrossProfit">
-            <template #default="{ row }">{{ row.openGrossProfit }}%</template>
+            <template #default="{ row }">
+              {{ row.operationType === 0 ? '-' : `${row.openGrossProfit}%` }}
+            </template>
+          </el-table-column>
+          <el-table-column label="Rating≥" min-width="100" prop="openRating">
+            <template #default="{ row }">
+              {{ row.operationType === 0 ? '-' : row.openRating }}
+            </template>
           </el-table-column>
         </el-table-column>
         <el-table-column label="关广告设定">
-          <el-table-column label="断货天数≥" min-width="100" prop="closeOutStockDays" />
-          <el-table-column label="剩余可售天数≤" min-width="120" prop="closeDays" />
-          <el-table-column label="可售库存数≤" min-width="110" prop="closeStock" />
+          <el-table-column label="断货天数≥" min-width="100" prop="closeOutStockDays">
+            <template #default="{ row }">
+              {{ row.operationType === 1 ? '-' : row.closeOutStockDays }}
+            </template>
+          </el-table-column>
+          <el-table-column label="剩余可售天数≤" min-width="125" prop="closeDays">
+            <template #default="{ row }">
+              {{ row.operationType === 1 ? '-' : row.closeDays }}
+            </template>
+          </el-table-column>
+          <el-table-column label="可售库存数≤" min-width="110" prop="closeStock">
+            <template #default="{ row }">
+              {{ row.operationType === 1 ? '-' : row.closeStock }}
+            </template>
+          </el-table-column>
           <el-table-column label="毛利率≤" min-width="110" prop="closeGrossProfit">
-            <template #default="{ row }">{{ row.closeGrossProfit }}%</template>
+            <template #default="{ row }">
+              {{ row.operationType === 1 ? '-' : `${row.closeGrossProfit}%` }}
+            </template>
+          </el-table-column>
+          <el-table-column label="Rating≤" min-width="100" prop="closeRating">
+            <template #default="{ row }">
+              {{ row.operationType === 1 ? '-' : row.closeRating }}
+            </template>
           </el-table-column>
         </el-table-column>
-
         <template #empty>
           <el-empty class="vab-data-empty" description="暂无数据" style="min-height: 200px" />
         </template>
