d1ef19e6ede9aab19db8e795f042c13a0b2f1c82
gaoyan.ma <gaoyan.ma@yunzhou.design>
2025-11-21 18:10:31 +0800

    fix: 零件库，零件报关，添加零件添加历史sku列

--- src/views/purchase/vabAutoComponents/vabAddComponent.vue
+++ src/views/purchase/vabAutoComponents/vabAddComponent.vue
@@ -1,53 +1,67 @@
 <template>
-  <vab-dialog 
-    v-model="dflag" 
-    style="width: fit-content" 
-    title="添加零件"
-    top="5%"
-    width="60%"
-    @close="handlerCloseDialog"
-  >
+  <vab-dialog v-model="dflag" style="width: fit-content" title="添加零件" top="5%" width="60%" @close="handlerCloseDialog">
     <vab-query-form>
       <vab-query-form-right-panel :span="24">
         <el-form inline :model="queryForm" @submit.prevent>
           <el-form-item>
-            <el-input v-model.trim="queryForm.keyWord" clearable placeholder="请输入搜索关键词" @input="queryData" @keyup.enter="queryData" />
+            <el-input
+              v-model.trim="queryForm.keyWord"
+              clearable
+              placeholder="请输入搜索关键词"
+              @input="queryData"
+              @keyup.enter="queryData"
+            />
           </el-form-item>
           <el-form-item>
-            <el-button :icon="Search" :loading="listLoading" native-type="submit" type="primary" @click="queryData"/>
+            <el-button :icon="Search" :loading="listLoading" native-type="submit" type="primary" @click="queryData" />
           </el-form-item>
         </el-form>
       </vab-query-form-right-panel>
     </vab-query-form>
 
-    <el-table 
-      ref="tableRef" 
-      border :cell-class-name="getCellStyle" 
+    <el-table
+      ref="tableRef"
+      border
+      :cell-class-name="getCellStyle"
       :cell-style="cellStyle"
       :data="list"
       :header-cell-style="{ 'text-align': 'center' }"
       max-height="650px"
       stripe
     >
-      <el-table-column  class="image-wall" label="图片" width="75">
+      <el-table-column class="image-wall" label="图片" width="75">
         <template #default="{ row }">
-          <el-image fit="fill" :src="row.imageUrl" style="display: block; width: 75px; height: 75px" @click="showPreviewImage(row.imageUrl)">
+          <el-image
+            fit="fill"
+            :src="row.imageUrl"
+            style="display: block; width: 75px; height: 75px"
+            @click="showPreviewImage(row.imageUrl)"
+          >
             <template #error><el-icon /></template>
           </el-image>
         </template>
       </el-table-column>
-      <el-table-column label="零件ID" prop="id" width="100"/>
-      <el-table-column label="SKU" prop="sku" :width="flexColumnWidth(list, 'SKU', 'sku')"/>
-      <el-table-column label="供应商" prop="suppliser" :width="flexColumnWidth(list, '供应商', 'suppliser')" />
-      <el-table-column label="零件名" prop="componentName" :width="flexColumnWidth(list, '零件名', 'componentName')"/>
-      <el-table-column label="单个SKU添加数量" prop="count" width="160">
+      <el-table-column label="零件ID" prop="id" width="100" />
+      <el-table-column label="SKU" min-width="350" prop="sku">
         <template #default="{ row }">
-            <el-input v-model="row.count" clearable />
+          <span class="history-sku-text">{{ row.sku }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="单位" prop="unit" width="130"/>
+      <el-table-column label="供应商" prop="suppliser" :width="flexColumnWidth(list, '供应商', 'suppliser')" />
+      <el-table-column label="零件名" prop="componentName" :width="flexColumnWidth(list, '零件名', 'componentName')" />
+      <el-table-column label="单个SKU添加数量" prop="count" width="160">
+        <template #default="{ row }">
+          <el-input v-model="row.count" clearable />
+        </template>
+      </el-table-column>
+      <el-table-column label="单位" prop="unit" width="90" />
+      <el-table-column label="历史SKU" min-width="350" prop="historySku">
+        <template #default="{ row }">
+          <span class="history-sku-text">{{ row.historySku }}</span>
+        </template>
+      </el-table-column>
       <template #empty>
-          <el-empty class="vab-data-empty" description="暂无数据" />
+        <el-empty class="vab-data-empty" description="暂无数据" />
       </template>
     </el-table>
 
@@ -58,13 +72,13 @@
       @current-change="handleCurrentChange"
       @size-change="handleSizeChange"
     />
-  
+
     <template #footer>
       <el-button type="danger" @click="handlerCloseDialog">取消</el-button>
       <el-button type="primary" @click="handleConfirm">确认</el-button>
     </template>
   </vab-dialog>
-  <el-image-viewer v-if="imagePreviewVisible" hide-on-click-modal :url-list="imagePreviewList" @close="imagePreviewClose"/>
+  <el-image-viewer v-if="imagePreviewVisible" hide-on-click-modal :url-list="imagePreviewList" @close="imagePreviewClose" />
 </template>
 
 <script lang="ts" setup>
@@ -74,24 +88,26 @@ import { flexColumnWidth } from '~/src/utils/tableColum'
 import { getAddComponentList, queryPoSkuComponentList } from '/@/api/devlocal/purchasePo'
 
 defineOptions({
-  name: 'VabAddComponent'
+  name: 'VabAddComponent',
 })
 const props = defineProps<{
   createComponentVisible: boolean
   sku?: string
   type?: string
-}>();
+}>()
 const dflag = ref<boolean>(false)
 
-watch(() => props.createComponentVisible, (newVal) => {
-  dflag.value = newVal
-  if (dflag.value === true) {
-    // 只在首次打开时加载数据
-    queryForm.keyWord = ''
-    typeInit()
-    
+watch(
+  () => props.createComponentVisible,
+  (newVal) => {
+    dflag.value = newVal
+    if (dflag.value === true) {
+      // 只在首次打开时加载数据
+      queryForm.keyWord = ''
+      typeInit()
+    }
   }
-})
+)
 
 const typeInit = () => {
   if (props.type === 'po') {
@@ -107,7 +123,7 @@ const typeInit = () => {
 //     fetchData()
 //   }
 // })
-const getCellStyle = (data: { row: any, column: any, rowIndex: number, columnIndex: number }) => {
+const getCellStyle = (data: { row: any; column: any; rowIndex: number; columnIndex: number }) => {
   if (data.columnIndex === 0) {
     return 'clear-padding'
   }
@@ -121,7 +137,7 @@ const total = ref<number>(0)
 const queryForm = reactive<any>({
   pageNo: 1,
   pageSize: 20,
-  keyWord: ''
+  keyWord: '',
 })
 const handleSizeChange = (value: number) => {
   queryForm.pageNo = 1
@@ -145,30 +161,30 @@ const emit = defineEmits(['update:createComponentVisible', 'update:tableValue'])
 
 const handlerCloseDialog = () => {
   dflag.value = false
-  emit('update:createComponentVisible', dflag.value);
+  emit('update:createComponentVisible', dflag.value)
 }
 const validateComponent = (item: any) => {
   if (item.count) {
-    return true;
-  } 
-  return false; 
-};
+    return true
+  }
+  return false
+}
 const handleConfirm = () => {
-  const countAllValid = list.value.some((item: any) => validateComponent(item));
+  const countAllValid = list.value.some((item: any) => validateComponent(item))
   if (countAllValid) {
     emit('update:tableValue', list.value)
     dflag.value = false
-    emit('update:createComponentVisible', dflag.value);
+    emit('update:createComponentVisible', dflag.value)
   } else {
-    $baseMessage('至少填写一个零件的添加数量', 'warning');
+    $baseMessage('至少填写一个零件的添加数量', 'warning')
   }
 }
 
-const cellStyle = (data: { row: any, column: any, rowIndex: number, columnIndex: number }):any => {
-  if (data.columnIndex === 0 || data.columnIndex === 1 || data.columnIndex === 6) {        
+const cellStyle = (data: { row: any; column: any; rowIndex: number; columnIndex: number }): any => {
+  if (data.columnIndex === 0 || data.columnIndex === 1 || data.columnIndex === 6) {
     return {
-      textAlign:'center'
-    } 
+      textAlign: 'center',
+    }
   }
 }
 
@@ -177,8 +193,8 @@ const imagePreviewList = ref<string[]>([])
 // 控制预览图片的隐藏显示
 const imagePreviewVisible = ref<boolean>(false)
 // 图片预览关闭事件
-const imagePreviewClose = () =>{
-  imagePreviewVisible.value = false;
+const imagePreviewClose = () => {
+  imagePreviewVisible.value = false
 }
 
 const showPreviewImage = (url: string) => {
@@ -187,8 +203,8 @@ const showPreviewImage = (url: string) => {
   imagePreviewList.value.push(url)
 }
 /**
-* 获取样品进度数据
-*/
+ * 获取样品进度数据
+ */
 const fetchData = async () => {
   listLoading.value = true
   const req = queryForm
@@ -225,8 +241,8 @@ onActivated(() => {
 </script>
 
 <style lang="scss" scoped>
-.moldDialog  { 
-  .el-dialog__body{
+.moldDialog {
+  .el-dialog__body {
     padding-top: 0px;
   }
 }
@@ -235,10 +251,10 @@ onActivated(() => {
   flex-direction: column;
   height: calc(80vh - 192px);
   max-height: calc(80vh - 192px);
-  
+
   .el-table {
-      flex: 1; // 使表格占据剩余空间
-      overflow: auto; // 确保表格内容可以滚动
+    flex: 1; // 使表格占据剩余空间
+    overflow: auto; // 确保表格内容可以滚动
   }
 }
 // // 设置行高
@@ -246,8 +262,8 @@ onActivated(() => {
 //   max-height: 69.8px !important;
 // }
 .custom-checkbox {
-transform: scale(1.2); 
-transform-origin: center;
+  transform: scale(1.2);
+  transform-origin: center;
 }
 .none {
   display: none;
@@ -259,5 +275,9 @@ transform-origin: center;
 .el-table :deep(.clear-padding) {
   padding-top: 0;
   padding-bottom: 0;
+}
+.history-sku-text {
+  white-space: pre-wrap;
+  word-break: break-all;
 }
 </style>
