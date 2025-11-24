f2513373f307552d7d5110c210d6cde218ce0ba5
gaoyan.ma <gaoyan.ma@yunzhou.design>
2025-11-24 10:47:29 +0800

    fix: 修改添加自动付款单选

--- src/views/purchase/vabAutoComponents/VabAddAutomaticPayment.vue
+++ src/views/purchase/vabAutoComponents/VabAddAutomaticPayment.vue
@@ -4,7 +4,14 @@
       <el-form-item label="路径" prop="path">
         <el-input v-model="form.path" clearable />
       </el-form-item>
-      <el-form-item label="付款比例" prop="percent">
+      <el-form-item label="付款类型">
+        <el-radio-group v-model="form.type">
+          <el-radio :value="0">付全款</el-radio>
+          <el-radio :value="1">付尾款</el-radio>
+          <el-radio :value="2">按比例</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item v-if="showPercent" label="付款比例" prop="percent">
         <el-input v-model="form.percent" clearable>
           <template #append>%</template>
         </el-input>
@@ -42,14 +49,36 @@ const formRef = ref<FormInstance>()
 const form = reactive<{
   path: string | null
   percent: string | null
+  type: number
 }>({
   path: null,
   percent: null,
+  type: 2,
+})
+
+const showPercent = computed(() => {
+  return form.type === 2
 })
 
 const rules = reactive<FormRules>({
   path: [{ required: true, message: '请输入路径！', trigger: 'blur' }],
-  percent: [{ required: true, message: '请输入付款比例！', trigger: 'blur' }],
+  percent: [
+    {
+      validator: (_rule, value, callback) => {
+        // 只有当选择"按比例"时，付款比例才是必填的
+        if (form.type === 2) {
+          if (!value || value.trim() === '') {
+            callback(new Error('请输入付款比例！'))
+          } else {
+            callback()
+          }
+        } else {
+          callback()
+        }
+      },
+      trigger: 'blur',
+    },
+  ],
 })
 
 const handleConfirm = async () => {
