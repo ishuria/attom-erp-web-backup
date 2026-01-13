0f966723d3200e15acbcd59ded33308d34950cc9
gaoyan.ma <gaoyan.ma@yunzhou.design>
2026-01-13 13:25:28 +0800

    fix: 修改检索后的展示宽度问题

--- src/views/storeOperations/vabAutoComponents/PerformanceTable.vue
+++ src/views/storeOperations/vabAutoComponents/PerformanceTable.vue
@@ -233,7 +233,7 @@ const handleWidth = (item: any) => {
   if (props.activeName === 0) {
     switch (item.label) {
       case 'SKU': {
-        return flexColumnWidth(props.data, 'SKU', 'sku') + 50
+        return Math.max(flexColumnWidth(props.data, 'SKU', 'sku') + 50, 300)
       }
       case 'ASIN': {
         return flexColumnWidth(props.data, 'ASIN', 'asin')
