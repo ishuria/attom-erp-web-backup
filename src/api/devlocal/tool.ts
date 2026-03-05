d57e13c0a0b3c34f83d61892a2ac63a171995dc8
gaoyan.ma <gaoyan.ma@yunzhou.design>
2026-03-05 13:48:40 +0800

    feat: 对接电子发票导入接口

--- src/api/devlocal/tool.ts
+++ src/api/devlocal/tool.ts
@@ -2,9 +2,21 @@ import request from '/@/utils/request'
 
 import { BASE_API } from '/@/api/devlocal/api'
 
-export function pdfMerge(data?: FormData) {
+export function pdfMerge(data?: FormData): Promise<Blob> {
   return request({
     url: `${BASE_API}/pdf/merge`,
+    method: 'post',
+    data,
+    responseType: 'blob',
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  })
+}
+
+export function invoiceImport(data?: FormData): Promise<Blob> {
+  return request({
+    url: `${BASE_API}/invoice/import`,
     method: 'post',
     data,
     responseType: 'blob',
